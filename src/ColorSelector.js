a53fb6f74fbf6ff135ecce4012171d9ec6b064b1
github username <github email address>
2019-10-17 16:02:09 -0700

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
-  
+  // provide the <div>s it is rendering with some way to fire off that method we wrote for Matrix (hint: consider using an event listener and controlling the argument when an event handler is fired)
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      const x = () => {this.props.setSelectedColor(str)}
+      return <div key={idx} onClick={x} className="color-swatch" style={{backgroundColor: str}}/>
     })
   )
   
